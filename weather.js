a0b83d380e6559c6f2a26a2122caff0b0773a65a
Pete Low <pedritolow@gmail.com>
2020-08-14 21:33:36 -0700

    added buttons function

--- weather.js
+++ weather.js
@@ -1,95 +1,130 @@
 $(document).ready(function() {
+let cities = JSON.parse(localStorage.getItem("cities")) || [];
 const apiKey = "c1759e335cae47d4b635c175d1bbe0ba";
-const localStorageKey = "cities"; 
 
 $("#search-button").on("click", function(){
 
-  var searchedCity = $("#get-city").val();
+  const searchedCity = $("#get-city").val();
   getCityWeather(searchedCity)
   console.log(searchedCity)
 } )
+
+localStorage.getItem("cities");
+console.log(JSON.parse(localStorage.getItem("cities")))
+
+function makeButtons(){
+  $("#cities-list").html("")
+  for(i = 0; i < cities.length; i++){
+  
+    $("#cities-list").append(`<button class="cities" id="${cities[i]}">${cities[i]}</button>`)
+    console.log(cities)
+    $(".cities").on("click", function(){
+    var buttonAttr =  $(this).attr("id");
+    getCityWeather(buttonAttr);
+    })
+  }
+}
+makeButtons();
 
 function getCityWeather(city) {
   $.ajax({
     method: "GET",
     url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
     }).then(function(response){
-      var cities = [];
-      var value = localStorage.getItem(localStorageKey);
-      if(value){
-        cities = JSON.parse(value)
-      }
       cities.unshift(city)
-      localStorage.setItem(localStorageKey, JSON.stringify(cities));
+      localStorage.setItem("cities", JSON.stringify(cities));
       console.log(cities)
 
-    var temp = (((response.main.temp - 273.15) * (9/5) + 32).toFixed(0));
-    var weatherIcon = response.weather[0].icon;
-    var humidity = response.main.humidity;
-    var windSpeed = response.wind.speed;
-    var currentWeather = response.weather[0].description;
-    var cityLatitude = response.coord.lat;
-    var cityLongitude = response.coord.lon;
-    var uvIndex = response
+      let cityLatitude = response.coord.lat;
+      let cityLongitude = response.coord.lon;
     
     $("#city").html(city);
+    // makeButtons();
+
+    displayWeatherForecast(cityLatitude,cityLongitude);
+    currentWeatherDisplay(cityLatitude,cityLongitude);
+  }) 
+}
+getCityWeather(cities[0]);
+
+
+function currentWeatherDisplay (latitude,longitude){
+  $.ajax({
+    method: "GET",
+    url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey
+  }).then(function(response){
+    const temp = (((response.current.temp - 273.15) * (9/5) + 32).toFixed(0));
+    const humidity = response.current.humidity;
+    const windSpeed = response.current.wind_speed;
+    const currentWeather = response.current.weather[0].description;
+    const uvIndex = response.current.uvi;
+    const weatherIcon = response.daily[0].weather[0].icon;
+
+    const currentIcon = ( src="http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+    console.log(currentIcon)
+
     $("#temp").html(temp);
     $("#humidity").html(humidity);
     $("#windspeed").html(windSpeed);
     $("#current-cond").html(currentWeather);
-    $("#icon").html(weatherIcon);
-
-    displayWeatherForecast(cityLatitude,cityLongitude);
-  }) 
+    $("#uv-index").html(uvIndex);
+    $("#current-icon").html(currentIcon);
+    
+    if (uvIndex < 3) {
+      $("#uv-index").css("background-color", "green");
+    } else if (uvIndex < 6) {
+      $("#uv-index").css("background-color", "yellow");
+    } else if (uvIndex < 8) {
+      $("#uv-index").css("background-color", "orange");
+    } else if (uvIndex < 11) {
+      $("#uv-index").css("background-color", "red");
+    } else {
+      $("#uv-index").css("background-color", "white");
+    } 
+  })
 }
+
 function displayWeatherForecast(latitude,longitude){
   $.ajax({
     method: "GET",
     url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey 
   }).then(function(response){
-    var forecastRow = $("#forecast-row");
+    const forecastRow = $("#forecast-row");
     forecastRow.empty();
 
     for(var i = 1; i < 6; i++){ 
-      var fDate = new Date(response.daily[i].dt * 1000) 
-      var fTemp = (((response.daily[i].temp.max - 273.15) * (9/5) + 32).toFixed(0));
-      var fHumidity = response.daily[i].humidity;
-      var fCurrentWeather = response.daily[i].weather[0].description;
-      var fIcon = response.daily[i].weather[0].icon;
-      var fUVI = response.daily[i].uvi;
-      var fWindSpeed = response.daily[i].wind_speed;
-
-      var data = document.createElement("td");
+      const fDate = new Date(response.daily[i].dt * 1000) 
+      const fTemp = (((response.daily[i].temp.max - 273.15) * (9/5) + 32).toFixed(0));
+      const fLowTemp = (((response.daily[i].temp.min - 273.15) * (9/5) + 32).toFixed(0));
+      const fHumidity = response.daily[i].humidity;
+      const fIcon = response.daily[i].weather[0].icon;
       
-      var dateHeader = document.createElement("h3")
+      const data = document.createElement("td");
+      
+      const dateHeader = document.createElement("h3")
       dateHeader.innerHTML = fDate.toLocaleDateString()
       data.append(dateHeader)
       
-      var icon = document.createElement("img")
+      const icon = document.createElement("img")
       icon.setAttribute("src","http://openweathermap.org/img/wn/" + fIcon + "@2x.png")
       data.append(icon); 
       
-      var tempSpan = document.createElement("div")
-      tempSpan.innerHTML = "Temp: " + fTemp;
+      const tempSpan = document.createElement("div")
+      tempSpan.innerHTML = `High Temp: ${fTemp} °`
       data.append(tempSpan);
       
-      var humiditySpan = document.createElement("div");
-      humiditySpan.innerHTML = "Humidity: " + fHumidity + "%";
+      const lowTempSpan = document.createElement("div")
+      lowTempSpan.innerHTML = `Low Temp ${fLowTemp}°`
+      data.append(lowTempSpan)
+      
+      const humiditySpan = document.createElement("div");
+      humiditySpan.innerHTML = `Humidity ${fHumidity}%`;
       data.append(humiditySpan);
       
-      
-
       forecastRow.append(data);
       
-    }
-
-    
+    }  
   })
-  
-
 }
-
 })
-
-// https://api.openweathermap.org/data/2.5/onecall?lat=47.61&lon=-122.33&appid=c1759e335cae47d4b635c175d1bbe0ba
 
